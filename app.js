d9cd527539718c07145c09a77d6dcc8b54ffb4a5
Quinton Fults <quinton.fults.work@gmail.com>
2021-06-03 13:07:37 -0700

    code

--- app.js
+++ app.js
@@ -13,13 +13,37 @@
 
 // dog.bark()
 
-function User (name, username, email, password) {
-  this.name = name
-  this.username = username
-  this.email = email
-  this.password = password
-  this.level = 'standard user'
-  this.printInfo = function () {
+// function User (name, username, email, password) {
+//   this.name = name
+//   this.username = username
+//   this.email = email
+//   this.password = password
+// this.level = 'standard user'
+// this.printInfo = function () {
+//   console.log(`
+//     Name: ${this.name}
+//     Username: ${this.username}
+//     Email: ${this.email}
+//     Password: ${this.password}
+//     Level: ${this.level}
+//   `)
+// }
+// }
+
+// const user1 = new User('John Doe', 'johndoe', 'johndoe@gmail.com', 'password1234')
+
+// user1.printInfo()
+
+class User {
+  constructor (name, username, email, password) {
+    this.name = name
+    this.username = username
+    this.email = email
+    this.password = password
+    this.level = 'standard user'
+  }
+
+  printInfo () {
     console.log(`
       Name: ${this.name}
       Username: ${this.username}
